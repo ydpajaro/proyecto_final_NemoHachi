7a8c54999dc47447282773a4c2cb5714208acf1f
ydpajaro <yeiner.pajaro@udea.edu.co>
2023-04-12 16:58:37 -0500

    Se crea el repositorio

--- app/src/main/java/com/yeinerdpajaro/nemohachi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/yeinerdpajaro/nemohachi/ui/splash/SplashActivity.kt
@@ -1,11 +1,11 @@
-package com.yeinerdpajaro.nemohachi
+package com.yeinerdpajaro.nemohachi.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
